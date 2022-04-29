aea41d5a2eb4b1c5f06dfb10e83af548efb80baa
LucianoNovak-Dedalord <51836716+LucianoDedalord@users.noreply.github.com>
2022-04-29 15:48:35 -0300

    muchas cosas

--- Assets/Random Gen/Spawner.cs
+++ Assets/Random Gen/Spawner.cs
@@ -6,8 +6,13 @@ public class Spawner : MonoBehaviour
 {
     public GameObject obj;
     public Transform ObjFather;
+    
+    
     public float SpawnTimeMax = 10.0f;
     public float SpawnTimeMin = 5.0f;
+
+    public Vector2 maxSpawnPos = Vector2.zero;
+    public Vector2 minSpawnPos = Vector2.zero;
 
     private void Start()
     {
@@ -17,7 +22,8 @@ public class Spawner : MonoBehaviour
     private IEnumerator Spawn()
     {
         float waitTime = Random.Range(SpawnTimeMin, SpawnTimeMax);
-        GameObject go = Instantiate(obj, transform.position, transform.rotation, ObjFather);
+        Vector2 spawnpos = new Vector2(Random.Range(minSpawnPos.x, maxSpawnPos.x), Random.Range(minSpawnPos.y, maxSpawnPos.y));
+        GameObject go = Instantiate(obj, spawnpos, transform.rotation, ObjFather);
         yield return new WaitForSeconds(waitTime);
         StartCoroutine("Spawn");
     }
